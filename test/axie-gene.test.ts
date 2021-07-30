540dbb13637bb384ec23a6183b69907cdbfbde33
ShaneMaglangit <shanedmaglangit@gmail.com>
2021-07-30 17:11:38 +0800

    Add test for mystic parts

--- test/axie-gene.test.ts
+++ test/axie-gene.test.ts
@@ -27,56 +27,56 @@ describe('AxieGene_Global', () => {
   });
   it('parse eyes', () => {
     const want = {
-      d: {partId: "eyes-chubby", cls: Cls.Beast, specialGenes: "", type: PartType.Eyes, name: "Chubby"},
-      r1: {partId: "eyes-chubby", cls: Cls.Beast, specialGenes: "", type: PartType.Eyes, name: "Chubby"},
-      r2: {partId: "eyes-blossom", cls: Cls.Plant, specialGenes: "", type: PartType.Eyes, name: "Blossom"},
+      d: { partId: 'eyes-chubby', cls: Cls.Beast, specialGenes: '', type: PartType.Eyes, name: 'Chubby' },
+      r1: { partId: 'eyes-chubby', cls: Cls.Beast, specialGenes: '', type: PartType.Eyes, name: 'Chubby' },
+      r2: { partId: 'eyes-blossom', cls: Cls.Plant, specialGenes: '', type: PartType.Eyes, name: 'Blossom' },
       mystic: false,
-    }
+    };
     expect(axieGene.eyes).toStrictEqual(want);
   });
   it('parse mouth', () => {
     const want = {
-      d: {partId: "mouth-tiny-turtle", cls: Cls.Reptile, specialGenes: "", type: PartType.Mouth, name: "Tiny Turtle"},
-      r1: {partId: "mouth-piranha", cls: Cls.Aquatic, specialGenes: "", type: PartType.Mouth, name: "Piranha"},
-      r2: {partId: "mouth-serious", cls: Cls.Plant, specialGenes: "", type: PartType.Mouth, name: "Serious"},
+      d: { partId: 'mouth-tiny-turtle', cls: Cls.Reptile, specialGenes: '', type: PartType.Mouth, name: 'Tiny Turtle' },
+      r1: { partId: 'mouth-piranha', cls: Cls.Aquatic, specialGenes: '', type: PartType.Mouth, name: 'Piranha' },
+      r2: { partId: 'mouth-serious', cls: Cls.Plant, specialGenes: '', type: PartType.Mouth, name: 'Serious' },
       mystic: false,
-    }
+    };
     expect(axieGene.mouth).toStrictEqual(want);
   });
   it('parse ears', () => {
     const want = {
-      d: {partId: "ears-lotus", cls: Cls.Plant, specialGenes: "", type: PartType.Ears, name: "Lotus"},
-      r1: {partId: "ears-nut-cracker", cls: Cls.Beast, specialGenes: "", type: PartType.Ears, name: "Nut Cracker"},
-      r2: {partId: "ears-inkling", cls: Cls.Aquatic, specialGenes: "", type: PartType.Ears, name: "Inkling"},
+      d: { partId: 'ears-lotus', cls: Cls.Plant, specialGenes: '', type: PartType.Ears, name: 'Lotus' },
+      r1: { partId: 'ears-nut-cracker', cls: Cls.Beast, specialGenes: '', type: PartType.Ears, name: 'Nut Cracker' },
+      r2: { partId: 'ears-inkling', cls: Cls.Aquatic, specialGenes: '', type: PartType.Ears, name: 'Inkling' },
       mystic: false,
-    }
+    };
     expect(axieGene.ears).toStrictEqual(want);
   });
   it('parse horn', () => {
     const want = {
-      d: {partId: "horn-rose-bud", cls: Cls.Plant, specialGenes: "", type: PartType.Horn, name: "Rose Bud"},
-      r1: {partId: "horn-caterpillars", cls: Cls.Bug, specialGenes: "", type: PartType.Horn, name: "Caterpillars"},
-      r2: {partId: "horn-dual-blade", cls: Cls.Beast, specialGenes: "", type: PartType.Horn, name: "Dual Blade"},
+      d: { partId: 'horn-rose-bud', cls: Cls.Plant, specialGenes: '', type: PartType.Horn, name: 'Rose Bud' },
+      r1: { partId: 'horn-caterpillars', cls: Cls.Bug, specialGenes: '', type: PartType.Horn, name: 'Caterpillars' },
+      r2: { partId: 'horn-dual-blade', cls: Cls.Beast, specialGenes: '', type: PartType.Horn, name: 'Dual Blade' },
       mystic: false,
-    }
+    };
     expect(axieGene.horn).toStrictEqual(want);
   });
   it('parse back', () => {
     const want = {
-      d: {partId: "back-balloon", cls: Cls.Bird, specialGenes: "", type: PartType.Back, name: "Balloon"},
-      r1: {partId: "back-jaguar", cls: Cls.Beast, specialGenes: "", type: PartType.Back, name: "Jaguar"},
-      r2: {partId: "back-jaguar", cls: Cls.Beast, specialGenes: "", type: PartType.Back, name: "Jaguar"},
+      d: { partId: 'back-balloon', cls: Cls.Bird, specialGenes: '', type: PartType.Back, name: 'Balloon' },
+      r1: { partId: 'back-jaguar', cls: Cls.Beast, specialGenes: '', type: PartType.Back, name: 'Jaguar' },
+      r2: { partId: 'back-jaguar', cls: Cls.Beast, specialGenes: '', type: PartType.Back, name: 'Jaguar' },
       mystic: false,
-    }
+    };
     expect(axieGene.back).toStrictEqual(want);
   });
   it('parse tail', () => {
     const want = {
-      d: {partId: "tail-ant", cls: Cls.Bug, specialGenes: "", type: PartType.Tail, name: "Ant"},
-      r1: {partId: "tail-hot-butt", cls: Cls.Plant, specialGenes: "", type: PartType.Tail, name: "Hot Butt"},
-      r2: {partId: "tail-swallow", cls: Cls.Bird, specialGenes: "", type: PartType.Tail, name: "Swallow"},
+      d: { partId: 'tail-ant', cls: Cls.Bug, specialGenes: '', type: PartType.Tail, name: 'Ant' },
+      r1: { partId: 'tail-hot-butt', cls: Cls.Plant, specialGenes: '', type: PartType.Tail, name: 'Hot Butt' },
+      r2: { partId: 'tail-swallow', cls: Cls.Bird, specialGenes: '', type: PartType.Tail, name: 'Swallow' },
       mystic: false,
-    }
+    };
     expect(axieGene.tail).toStrictEqual(want);
   });
 });
@@ -103,56 +103,62 @@ describe('AxieGene_Japan', () => {
   });
   it('parse eyes', () => {
     const want = {
-      d: {partId: "eyes-dokuganryu", cls: Cls.Reptile, specialGenes: "japan", type: PartType.Eyes, name: "Dokuganryu"},
-      r1: {partId: "eyes-mavis", cls: Cls.Bird, specialGenes: "", type: PartType.Eyes, name: "Mavis"},
-      r2: {partId: "eyes-telescope", cls: Cls.Aquatic, specialGenes: "", type: PartType.Eyes, name: "Telescope"},
+      d: {
+        partId: 'eyes-dokuganryu',
+        cls: Cls.Reptile,
+        specialGenes: 'japan',
+        type: PartType.Eyes,
+        name: 'Dokuganryu',
+      },
+      r1: { partId: 'eyes-mavis', cls: Cls.Bird, specialGenes: '', type: PartType.Eyes, name: 'Mavis' },
+      r2: { partId: 'eyes-telescope', cls: Cls.Aquatic, specialGenes: '', type: PartType.Eyes, name: 'Telescope' },
       mystic: false,
-    }
+    };
     expect(axieGene.eyes).toStrictEqual(want);
   });
   it('parse mouth', () => {
     const want = {
-      d: {partId: "mouth-geisha", cls: Cls.Aquatic, specialGenes: "japan", type: PartType.Mouth, name: "Geisha"},
-      r1: {partId: "mouth-peace-maker", cls: Cls.Bird, specialGenes: "", type: PartType.Mouth, name: "Peace Maker"},
-      r2: {partId: "mouth-peace-maker", cls: Cls.Bird, specialGenes: "", type: PartType.Mouth, name: "Peace Maker"},
+      d: { partId: 'mouth-geisha', cls: Cls.Aquatic, specialGenes: 'japan', type: PartType.Mouth, name: 'Geisha' },
+      r1: { partId: 'mouth-peace-maker', cls: Cls.Bird, specialGenes: '', type: PartType.Mouth, name: 'Peace Maker' },
+      r2: { partId: 'mouth-peace-maker', cls: Cls.Bird, specialGenes: '', type: PartType.Mouth, name: 'Peace Maker' },
       mystic: false,
-    }
+    };
     expect(axieGene.mouth).toStrictEqual(want);
   });
   it('parse ears', () => {
     const want = {
-      d: {partId: "ears-maiko", cls: Cls.Plant, specialGenes: "japan", type: PartType.Ears, name: "Maiko"},
-      r1: {partId: "ears-maiko", cls: Cls.Plant, specialGenes: "japan", type: PartType.Ears, name: "Maiko"},
-      r2: {partId: "ears-puppy", cls: Cls.Beast, specialGenes: "", type: PartType.Ears, name: "Puppy"},
+      d: { partId: 'ears-maiko', cls: Cls.Plant, specialGenes: 'japan', type: PartType.Ears, name: 'Maiko' },
+      r1: { partId: 'ears-maiko', cls: Cls.Plant, specialGenes: 'japan', type: PartType.Ears, name: 'Maiko' },
+      r2: { partId: 'ears-puppy', cls: Cls.Beast, specialGenes: '', type: PartType.Ears, name: 'Puppy' },
       mystic: false,
-    }
+    };
     expect(axieGene.ears).toStrictEqual(want);
   });
   it('parse horn', () => {
     const want = {
-      d: {partId: "horn-kendama", cls: Cls.Beast, specialGenes: "japan", type: PartType.Horn, name: "Kendama"},
-      r1: {partId: "horn-parasite", cls: Cls.Bug, specialGenes: "", type: PartType.Horn, name: "Parasite"},
-      r2: {partId: "horn-kendama", cls: Cls.Beast, specialGenes: "japan", type: PartType.Horn, name: "Kendama"},
+      d: { partId: 'horn-kendama', cls: Cls.Beast, specialGenes: 'japan', type: PartType.Horn, name: 'Kendama' },
+      r1: { partId: 'horn-parasite', cls: Cls.Bug, specialGenes: '', type: PartType.Horn, name: 'Parasite' },
+      r2: { partId: 'horn-kendama', cls: Cls.Beast, specialGenes: 'japan', type: PartType.Horn, name: 'Kendama' },
       mystic: false,
-    }
+    };
     expect(axieGene.horn).toStrictEqual(want);
   });
   it('parse back', () => {
     const want = {
-      d: {partId: "back-hamaya", cls: Cls.Beast, specialGenes: "japan", type: PartType.Back, name: "Hamaya"},
-      r1: {partId: "back-origami", cls: Cls.Bird, specialGenes: "japan", type: PartType.Back, name: "Origami"},
-      r2: {partId: "back-jaguar", cls: Cls.Beast, specialGenes: "", type: PartType.Back, name: "Jaguar"},
+      d: { partId: 'back-hamaya', cls: Cls.Beast, specialGenes: 'japan', type: PartType.Back, name: 'Hamaya' },
+      r1: { partId: 'back-origami', cls: Cls.Bird, specialGenes: 'japan', type: PartType.Back, name: 'Origami' },
+      r2: { partId: 'back-jaguar', cls: Cls.Beast, specialGenes: '', type: PartType.Back, name: 'Jaguar' },
       mystic: false,
-    }
+    };
     expect(axieGene.back).toStrictEqual(want);
   });
   it('parse tail', () => {
     const want = {
-      d: {partId: "tail-maki", cls: Cls.Bug, specialGenes: "japan", type: PartType.Tail, name: "Maki"},
-      r1: {partId: "tail-swallow", cls: Cls.Bird, specialGenes: "", type: PartType.Tail, name: "Swallow"},
-      r2: {partId: "tail-hare", cls: Cls.Beast, specialGenes: "", type: PartType.Tail, name: "Hare"},
+      d: { partId: 'tail-maki', cls: Cls.Bug, specialGenes: 'japan', type: PartType.Tail, name: 'Maki' },
+      r1: { partId: 'tail-swallow', cls: Cls.Bird, specialGenes: '', type: PartType.Tail, name: 'Swallow' },
+      r2: { partId: 'tail-hare', cls: Cls.Beast, specialGenes: '', type: PartType.Tail, name: 'Hare' },
       mystic: false,
-    }
+    };
     expect(axieGene.tail).toStrictEqual(want);
   });
 });
@@ -179,56 +185,106 @@ describe('AxieGene_Japan_512bit', () => {
   });
   it('parse eyes', () => {
     const want = {
-      d: {partId: "eyes-dokuganryu", cls: Cls.Reptile, specialGenes: "japan", type: PartType.Eyes, name: "Dokuganryu"},
-      r1: {partId: "eyes-mavis", cls: Cls.Bird, specialGenes: "", type: PartType.Eyes, name: "Mavis"},
-      r2: {partId: "eyes-telescope", cls: Cls.Aquatic, specialGenes: "", type: PartType.Eyes, name: "Telescope"},
+      d: {
+        partId: 'eyes-dokuganryu',
+        cls: Cls.Reptile,
+        specialGenes: 'japan',
+        type: PartType.Eyes,
+        name: 'Dokuganryu',
+      },
+      r1: { partId: 'eyes-mavis', cls: Cls.Bird, specialGenes: '', type: PartType.Eyes, name: 'Mavis' },
+      r2: { partId: 'eyes-telescope', cls: Cls.Aquatic, specialGenes: '', type: PartType.Eyes, name: 'Telescope' },
       mystic: false,
-    }
+    };
     expect(axieGene.eyes).toStrictEqual(want);
   });
   it('parse mouth', () => {
     const want = {
-      d: {partId: "mouth-geisha", cls: Cls.Aquatic, specialGenes: "japan", type: PartType.Mouth, name: "Geisha"},
-      r1: {partId: "mouth-peace-maker", cls: Cls.Bird, specialGenes: "", type: PartType.Mouth, name: "Peace Maker"},
-      r2: {partId: "mouth-peace-maker", cls: Cls.Bird, specialGenes: "", type: PartType.Mouth, name: "Peace Maker"},
+      d: { partId: 'mouth-geisha', cls: Cls.Aquatic, specialGenes: 'japan', type: PartType.Mouth, name: 'Geisha' },
+      r1: { partId: 'mouth-peace-maker', cls: Cls.Bird, specialGenes: '', type: PartType.Mouth, name: 'Peace Maker' },
+      r2: { partId: 'mouth-peace-maker', cls: Cls.Bird, specialGenes: '', type: PartType.Mouth, name: 'Peace Maker' },
       mystic: false,
-    }
+    };
     expect(axieGene.mouth).toStrictEqual(want);
   });
   it('parse ears', () => {
     const want = {
-      d: {partId: "ears-maiko", cls: Cls.Plant, specialGenes: "japan", type: PartType.Ears, name: "Maiko"},
-      r1: {partId: "ears-maiko", cls: Cls.Plant, specialGenes: "japan", type: PartType.Ears, name: "Maiko"},
-      r2: {partId: "ears-puppy", cls: Cls.Beast, specialGenes: "", type: PartType.Ears, name: "Puppy"},
+      d: { partId: 'ears-maiko', cls: Cls.Plant, specialGenes: 'japan', type: PartType.Ears, name: 'Maiko' },
+      r1: { partId: 'ears-maiko', cls: Cls.Plant, specialGenes: 'japan', type: PartType.Ears, name: 'Maiko' },
+      r2: { partId: 'ears-puppy', cls: Cls.Beast, specialGenes: '', type: PartType.Ears, name: 'Puppy' },
       mystic: false,
-    }
+    };
     expect(axieGene.ears).toStrictEqual(want);
   });
   it('parse horn', () => {
     const want = {
-      d: {partId: "horn-kendama", cls: Cls.Beast, specialGenes: "japan", type: PartType.Horn, name: "Kendama"},
-      r1: {partId: "horn-parasite", cls: Cls.Bug, specialGenes: "", type: PartType.Horn, name: "Parasite"},
-      r2: {partId: "horn-kendama", cls: Cls.Beast, specialGenes: "japan", type: PartType.Horn, name: "Kendama"},
+      d: { partId: 'horn-kendama', cls: Cls.Beast, specialGenes: 'japan', type: PartType.Horn, name: 'Kendama' },
+      r1: { partId: 'horn-parasite', cls: Cls.Bug, specialGenes: '', type: PartType.Horn, name: 'Parasite' },
+      r2: { partId: 'horn-kendama', cls: Cls.Beast, specialGenes: 'japan', type: PartType.Horn, name: 'Kendama' },
       mystic: false,
-    }
+    };
     expect(axieGene.horn).toStrictEqual(want);
   });
   it('parse back', () => {
     const want = {
-      d: {partId: "back-hamaya", cls: Cls.Beast, specialGenes: "japan", type: PartType.Back, name: "Hamaya"},
-      r1: {partId: "back-origami", cls: Cls.Bird, specialGenes: "japan", type: PartType.Back, name: "Origami"},
-      r2: {partId: "back-jaguar", cls: Cls.Beast, specialGenes: "", type: PartType.Back, name: "Jaguar"},
+      d: { partId: 'back-hamaya', cls: Cls.Beast, specialGenes: 'japan', type: PartType.Back, name: 'Hamaya' },
+      r1: { partId: 'back-origami', cls: Cls.Bird, specialGenes: 'japan', type: PartType.Back, name: 'Origami' },
+      r2: { partId: 'back-jaguar', cls: Cls.Beast, specialGenes: '', type: PartType.Back, name: 'Jaguar' },
       mystic: false,
-    }
+    };
     expect(axieGene.back).toStrictEqual(want);
   });
   it('parse tail', () => {
     const want = {
-      d: {partId: "tail-maki", cls: Cls.Bug, specialGenes: "japan", type: PartType.Tail, name: "Maki"},
-      r1: {partId: "tail-swallow", cls: Cls.Bird, specialGenes: "", type: PartType.Tail, name: "Swallow"},
-      r2: {partId: "tail-hare", cls: Cls.Beast, specialGenes: "", type: PartType.Tail, name: "Hare"},
+      d: { partId: 'tail-maki', cls: Cls.Bug, specialGenes: 'japan', type: PartType.Tail, name: 'Maki' },
+      r1: { partId: 'tail-swallow', cls: Cls.Bird, specialGenes: '', type: PartType.Tail, name: 'Swallow' },
+      r2: { partId: 'tail-hare', cls: Cls.Beast, specialGenes: '', type: PartType.Tail, name: 'Hare' },
       mystic: false,
-    }
+    };
     expect(axieGene.tail).toStrictEqual(want);
+  });
+});
+
+describe('AxieGene_Mystic', () => {
+  const axieGene = new AxieGene('0x3000400009204433cc231002c4210902c0210882146408860c621944cc221902');
+  it('parse eyes', () => {
+    expect(axieGene.eyes.mystic).toBe(true);
+  });
+  it('parse ears', () => {
+    expect(axieGene.ears.mystic).toBe(true);
+  });
+  it('parse mouth', () => {
+    expect(axieGene.mouth.mystic).toBe(true);
+  });
+  it('parse tail', () => {
+    expect(axieGene.tail.mystic).toBe(true);
+  });
+  it('parse mouth', () => {
+    expect(axieGene.back.mystic).toBe(false);
+  });
+  it('parse tail', () => {
+    expect(axieGene.horn.mystic).toBe(false);
+  });
+});
+
+describe('AxieGene_Mystic_512bit', () => {
+  const axieGene = new AxieGene('0x280000000000010040412090830C0000000101942040440A00010190284082040001018C2061000A000101801021400400010180204080060001018418404008', HexType.Bit512);
+  it('parse eyes', () => {
+    expect(axieGene.eyes.mystic).toBe(true);
+  });
+  it('parse ears', () => {
+    expect(axieGene.ears.mystic).toBe(true);
+  });
+  it('parse mouth', () => {
+    expect(axieGene.mouth.mystic).toBe(true);
+  });
+  it('parse tail', () => {
+    expect(axieGene.tail.mystic).toBe(true);
+  });
+  it('parse mouth', () => {
+    expect(axieGene.back.mystic).toBe(false);
+  });
+  it('parse tail', () => {
+    expect(axieGene.horn.mystic).toBe(false);
   });
 });
